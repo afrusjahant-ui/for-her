d54667443190deb2446edc300e3d8fbcdcda770e
afrusjahant-ui <afrusjahant@gmail.com>
2026-04-11 13:10:10 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="Special Message", page_icon="💖")
 
 # Title and Question
-st.title("Hi! Ekta kotha bolar chilo... 🥰")
+st.title("Wanna ask you a question🥰")
 st.subheader("Will you be my wifey?")
 
 # Cute GIF (apni চাইলে link change korte paren)
@@ -16,7 +16,8 @@ col1, col2 = st.columns(2)
 with col1:
     if st.button("Yes! ❤️"):
         st.balloons()
-        st.success("Yayyy! I knew it! ummmmahhhhh!alabiuuuuuu 🥰")
+        st.image("https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExemdzeGJjOG5sY3VnaHJnanQ3ZzFua3B6dDUweGlybmcxanJiMG0zMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/sC37EpvhNQ2GhXGwdc/giphy.gif")
+        st.success("Yayyy! I knew it! ummmmahhhhh! alabiuuuuuu 🥰")
 
 with col2:
     if st.button("No 💔"):
