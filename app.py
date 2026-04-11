04baf95e66f5b37317d0b16787144f725c7bd827
afrusjahant-ui <afrusjahant@gmail.com>
2026-04-11 13:44:29 +0600

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ if yes_click:
     # 2. Notun celebration content
     st.balloons()
     # Ekhane ekta celebrate korar moto GIF use kora hoyeche
-    new_gif = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExemdzeGJjOG5sY3VnaHJnanQ3ZzFua3B6dDUweGlybmcxanJiMG0zMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/sC37EpvhNQ2GhXGwdc/giphy.gif"
+    new_gif = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbjJvZTIzMHB1a2pkZGtsbHFxOGRrMjMyZnhiZzYwNTI2YXBkaXYwdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/901mxGLGQN2PyCQpoc/giphy.gif"
     st.image(new_gif)
     st.success("Yay! ummmmmmmahhhh babyyyyyy ❤️🥰")
 
